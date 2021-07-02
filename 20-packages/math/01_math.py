c476b97b8ff265b10772e02841b5fcd498d390a8
patvdc <patvdc@gmail.com>
2021-07-02 16:41:04 +0200

    package math

---
+++ 20-packages/math/01_math.py
@@ -0,0 +1,27 @@
+import math
+
+x = math.sqrt(81)
+print("sqrt =",x)
+
+x = math.ceil(3.4)
+print("ceil 3.4 =",x)
+
+x = math.floor(3.4)
+print("floor 3.4 =",x)
+
+# constant pi
+x = math.pi
+print("pi = ",x)
+
+x = math.trunc(4.99)
+print("trunc 4.99 =",x)
+
+x = math.trunc(-99.99)
+print("trunc 99.99 =",x)
+
+x = (2,3,4)
+print("product = ",math.prod(x))
+
+
+
+
